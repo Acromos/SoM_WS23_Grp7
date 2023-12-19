63a797721f5abbe7a16b69991dbdf197bf8a62c4
Diana0060 <151841635+Diana0060@users.noreply.github.com>
2023-12-19 18:08:21 +0100

    Add files via upload

--- Mess-Projekt/readMQTT.py
+++ Mess-Projekt/readMQTT.py
@@ -1,33 +1,34 @@
 from paho.mqtt import client as mqtt_client
 import time
 
-MQTT_PORT=1883
-MQTT_ADDRESS="141.22.194.198"
-MQTT_CLIENT_NAME="Test_PiSub"
-MQTT_TOPIC="Gruppenname/Kanal"
-TICK_RATE_HZ=2
-TICK_RATE=1/TICK_RATE_HZ
+# Parameterfestlegung
+MQTT_PORT=1883                  
+MQTT_ADDRESS="141.22.194.198"   # IP Adresse: 141.22.194.198/ Außerhalb der Hochschule unterscheidet sie sich
+MQTT_CLIENT_NAME="Diana_read"
+MQTT_TOPIC="MEDS/Kanal"
+TICK_RATE_HZ=2                  
+TICK_RATE=1/TICK_RATE_HZ        # Wie oft wird nach neuen Nachrichtenüberprüft
 
-#Liste für die Nachrichten
-message_queue = []
-#Funktion die beim eintreffen einer Nachricht diese in eine Liste packt
+# Liste für die Nachrichten zum Zwischenspeichern
+message_queue = []      
+# Funktion die beim eintreffen einer Nachricht diese in eine Liste packt
 def on_message(client, userdata, msg):
     message=msg.payload.decode()
     message_queue.append(message)
-#Verbinden mit MQTT
-client = mqtt_client.Client(MQTT_CLIENT_NAME)
-client.connect(MQTT_ADDRESS,MQTT_PORT)
-#Subscribe zum Thema und Funktion "on_message" hinzufügen
+# Verbinden mit MQTT
+client = mqtt_client.Client(MQTT_CLIENT_NAME)       # Erstellen des MQTT Clients
+client.connect(MQTT_ADDRESS,MQTT_PORT)              # Verbinden mit dem Broker
+# Subscribe zum Thema und Funktion "on_message" hinzufügen
 client.subscribe(MQTT_TOPIC)
 client.on_message = on_message
-#Für immer laufen lassen
+# Für immer laufen lassen: Start des MQTT Clients
 client.loop_start()
 while (True):
     last_value=""
     value_queue = []
-    #Kurz warten
+    # Kurz warten
     time.sleep(TICK_RATE)
-    #Wenn es noch neue Nachrichten gibt...
+    # Wenn es noch neue Nachrichten gibt...
     while len(message_queue)>0:
         #...dann kopiere diese von der Empfangsliste in die Anzeige-Liste
         last_value=message_queue.pop()
--- Mess-Projekt/sendMQTTShort.py
+++ Mess-Projekt/sendMQTTShort.py
@@ -3,11 +3,11 @@ from random import randint
 
 MQTT_PORT=1883
 MQTT_ADDRESS="141.22.194.198"
-MQTT_CLIENT_NAME="Test_PiPub"
-MQTT_TOPIC="Gruppenname/Kanal"
+MQTT_CLIENT_NAME="Diana_send"
+MQTT_TOPIC="MEDS/Kanal"
 
 #Hier steht die Nachricht
-text="Random Value: " + str(randint(1, 100))
+text="Hello World and Random Value: " + str(randint(1, 100))
 
 #Send Text to MQTT
 client = mqtt_client.Client(MQTT_CLIENT_NAME)
